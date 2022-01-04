1450c8cc35c0079eb41a79006b0a34e4bc238146
Slava Pascaru <slavapascaru@gmail.com>
2022-01-04 22:26:32 +0200

    Second commit

--- script1.js
+++ script1.js
@@ -1,4 +1,15 @@
-let cat = document.querySelector("#text");
-cat.addEventListener("click", function () {
-  text.innerHTML = "HELLO";
+let elem = document.querySelector("#text");
+elem.addEventListener("click", function () {
+  //   text.innerHTML = "HELLO";
+  elem.innerHTML = "<h1>HELLO</h1>";
+  elem.innerText = "<h1>Hi</h1>";
+  console.log(elem.classList);
+  elem.className = "i-am-a-new-class";
+  console.log(elem.classList);
+  elem.classList.add("new-class");
+  console.log(elem.classList);
+  elem.classList.remove("new-class");
+  console.log(elem.classList);
+
+  elem.style.fontSize = "30px";
 });
